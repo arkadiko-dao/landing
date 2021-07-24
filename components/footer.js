033b96765701d0a26f5d4211ae12a9231cb7e298
Philip De Smedt <philip.desmedt@gmail.com>
2021-07-24 13:55:36 +0200

    add tweaks & improvements

--- components/footer.js
+++ components/footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
             <div className="flex space-x-6 md:order-2 mt-6">
               <a href="https://arkadikofinance.medium.com/" className="text-gray-300 hover:text-white">
                 <span className="sr-only">Medium</span>
-                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" clip-rule="evenodd">
+                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" clipRule="evenodd">
                   <path
                     d="M2.846 6.887c.03-.295-.083-.586-.303-.784l-2.24-2.7v-.403h6.958l5.378 11.795 4.728-11.795h6.633v.403l-1.916 1.837c-.165.126-.247.333-.213.538v13.498c-.034.204.048.411.213.537l1.871 1.837v.403h-9.412v-.403l1.939-1.882c.19-.19.19-.246.19-.537v-10.91l-5.389 13.688h-.728l-6.275-13.688v9.174c-.052.385.076.774.347 1.052l2.521 3.058v.404h-7.148v-.404l2.521-3.058c.27-.279.39-.67.325-1.052v-10.608z"
                   ></path>
@@ -29,9 +29,9 @@ export default function Footer() {
                 <span className="sr-only">GitHub</span>
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
               </a>
@@ -44,7 +44,7 @@ export default function Footer() {
               <a href="" className="text-gray-300 hover:text-white">
                 <span className="sr-only">Discourse</span>
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path d="M6.45 18.2l.23-.06-.23.05z" fill="#fff"/><path fill-rule="evenodd" clip-rule="evenodd" d="M3 11.81a9.6 9.6 0 019.48-9.62c5.14 0 9.31 4.48 9.31 9.79 0 5.31-4.17 9.79-9.32 9.79H3v-9.96zm3.46 6.35l.68-3.53a6.12 6.12 0 011.13-8.01 5.65 5.65 0 017.78.5c.42.32.79.7 1.1 1.13l.01.01a6.1 6.1 0 01.64 7.9 5.62 5.62 0 01-7.54 1.57l-3.81.46v-.03z" fill="#fff"/>
+                  <path d="M6.45 18.2l.23-.06-.23.05z" fill="#fff"/><path fillRule="evenodd" clipRule="evenodd" d="M3 11.81a9.6 9.6 0 019.48-9.62c5.14 0 9.31 4.48 9.31 9.79 0 5.31-4.17 9.79-9.32 9.79H3v-9.96zm3.46 6.35l.68-3.53a6.12 6.12 0 011.13-8.01 5.65 5.65 0 017.78.5c.42.32.79.7 1.1 1.13l.01.01a6.1 6.1 0 01.64 7.9 5.62 5.62 0 01-7.54 1.57l-3.81.46v-.03z" fill="#fff"/>
                 </svg>
               </a>
             </div>
